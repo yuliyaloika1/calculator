45dccc955092c2a29f527479fe06b9eeb4fdf221
Yuliya Loika <yuliya_loika@epam.com>
2021-06-17 15:39:04 +0300

    commit with report

--- app/calculator.js
+++ app/calculator.js
@@ -7,4 +7,4 @@ const add = (x, y) => {
   module.exports = {
     add,
     multiply,
-  };
+  }
--- test/specs/calculator_test.spec.js
+++ test/specs/calculator_test.spec.js
@@ -4,12 +4,12 @@ const {expect} = require('chai');
 describe('calculator', function() {
   describe('add()', function() {
     it('should add 2 numbers together', function() {
-      expect(calculator.add(1, 4)).to.equal(5);
+      expect(calculator.add(1, 4)).to.be.equal(5);
     });
   });
   describe('multiply()', function() {
     it('should multiply 2 numbers together', function() {
-      expect(calculator.multiply(1, 4)).to.equal(5);
+      expect(calculator.multiply(1, 4)).to.be.equal(4);
     });
   });
 });
